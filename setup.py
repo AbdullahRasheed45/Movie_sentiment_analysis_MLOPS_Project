95141fff30641c4e33d33599c31291150220dbe0
Muhammad Abdullah Rasheed <abdullahrasheed45@gmail.com>
2025-07-05 12:03:49 +0100

    inital commit made

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the project of machine learning of the sentimental analysis of the movie review which will use the concept of the MLOPS and follow its work flow till the monitering using graffana and pometerus',
+    author='Muhammad Abdullah Rasheed',
+    license='',
+)
