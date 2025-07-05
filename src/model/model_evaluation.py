4f4b73411f64c15ced6476051c7921eed0c26037
Muhammad Abdullah Rasheed <abdullahrasheed45@gmail.com>
2025-07-05 12:44:14 +0100

    CI test 2

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -14,26 +14,26 @@ from src.logger import logging
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
+dagshub_url = "https://dagshub.com"
+repo_owner = "AbdullahRasheed45"
+repo_name = "Movie_sentiment_analysis_MLOPS"
 
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 # Below code block is for local use
-# -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/AbdullahRasheed45/Movie_sentiment_analysis_MLOPS.mlflow ')
-dagshub.init(repo_owner='AbdullahRasheed45', repo_name='Movie_sentiment_analysis_MLOPS', mlflow=True)
-# -------------------------------------------------------------------------------------
+# # -------------------------------------------------------------------------------------
+# mlflow.set_tracking_uri('https://dagshub.com/AbdullahRasheed45/Movie_sentiment_analysis_MLOPS.mlflow ')
+# dagshub.init(repo_owner='AbdullahRasheed45', repo_name='Movie_sentiment_analysis_MLOPS', mlflow=True)
+# # -------------------------------------------------------------------------------------
 
 
 def load_model(file_path: str):
