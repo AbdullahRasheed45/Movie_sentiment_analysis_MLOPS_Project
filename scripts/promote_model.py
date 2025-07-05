2344c3fc16e3c3e1f8d119fd7909987d53d4b15b
Muhammad Abdullah Rasheed <abdullahrasheed45@gmail.com>
2025-07-05 12:55:13 +0100

    CI test 3

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -13,8 +13,8 @@ def promote_model():
     os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
     dagshub_url = "https://dagshub.com"
-    repo_owner = "vikashdas770"
-    repo_name = "YT-Capstone-Project"
+    repo_owner = "AbdullahRasheed45"
+    repo_name = "Movie_sentiment_analysis_MLOPS"
 
     # Set up MLflow tracking URI
     mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
